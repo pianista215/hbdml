d4c540a2220508af22bd08a96d622acd6a261bee
Unai Sarasola <usarasola@stratio.com>
2017-05-26 13:31:12 +0200

    Configuration

--- ecommerce_web/webapp/build.sbt
+++ ecommerce_web/webapp/build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.12.2"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % "10.0.6",
+  "com.typesafe" % "config" % "1.3.1",
   "com.typesafe.akka" %% "akka-http-testkit" % "10.0.6" % Test
 )
     
--- ecommerce_web/webapp/src/main/scala/com/devsmobile/hbdml/webapp/WebServer.scala
+++ ecommerce_web/webapp/src/main/scala/com/devsmobile/hbdml/webapp/WebServer.scala
@@ -5,6 +5,8 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+
 import scala.io.StdIn
 
 object WebServer {
@@ -15,6 +17,8 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
+    val config = ConfigFactory.load()
+
     val route =
       path("") {
         get {
@@ -22,10 +26,12 @@ object WebServer {
         }
       }
 
+    val host = config.getString("webapp.host")
+    val port = config.getInt("webapp.port")
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, host, port)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    println(s"Server online at http://${host}:${port}/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
